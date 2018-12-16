d140e42dddfe33348366e714e307f3920c43f5e2
SoCooool <45396817+SoCooool@users.noreply.github.com>
2018-12-16 21:59:14 +0800

    1216

--- install/upgrade_base_10.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `dir_admins` ADD `ID` INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST;
-ALTER TABLE `dir_admins` ADD `Email` VARCHAR(60) NOT NULL;
-ALTER TABLE `dir_links` ADD `Admin_id` INT(10) DEFAULT '0' NOT NULL;
-DELETE FROM `dir_params` WHERE `Name`='thumb_width';
-DELETE FROM `dir_params` WHERE `Name`='thumb_height';
-INSERT INTO `dir_params`(`Name`,`VALUE`,`type`,`description`) VALUES ('default_admin_email','','text','Default Admin Email');
-ALTER TABLE `dir_params` ADD `order_display` INT( 5 ) DEFAULT '0' NOT NULL ;
